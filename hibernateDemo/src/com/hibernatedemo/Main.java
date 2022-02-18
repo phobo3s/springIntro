6fd6f9f594c5d086b8fd662f1ac902e1607d2db2
phobo3s <bberkkilinc@gmail.com>
2022-02-18 18:39:08 +0300

    Hibernate demo done

--- hibernateDemo/src/com/hibernatedemo/Main.java
+++ hibernateDemo/src/com/hibernatedemo/Main.java
@@ -12,7 +12,8 @@ public class Main {
 
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
 				.addAnnotatedClass(City.class).buildSessionFactory();
-	
+		
+		// Unit of work pattern
 		Session session = factory.getCurrentSession();
 		
 		session.beginTransaction();
@@ -20,16 +21,37 @@ public class Main {
 		//HQL - hibernate query language
 		//from City c where c.countryCode = 'TUR' Order By c.name DESC
 		//List<City> cities = session.createQuery("select City.countryCode from City c Group By c.countryCode").getResultList();
-		List<String> cCodes = session.createQuery("select c.countryCode from City c Group By c.countryCode").getResultList();
+		//List<String> cCodes = session.createQuery("select c.countryCode from City c Group By c.countryCode").getResultList();
+		
+		/*
+		 * City city = new City(); city.setName("Düzce 10"); city.setCountryCode("TUR");
+		 * city.setDistrict("Karadeniz"); city.setPopulation(103);
+		 * session.save(city);
+		 */
+		
+		//update
+		/*
+		 * City city = session.get(City.class, 4086); 
+		 * city.setPopulation(15);
+		 * session.save(city); 
+		 * System.out.println(city.getName());
+		 */
+		
+		//Delete
+		City city = session.get(City.class, 4086);
+		session.delete(city);
+		
+		
 		
 		session.getTransaction().commit();
+		System.out.println("Şehir Gitti");
 		
 		session.close();
 		factory.close();
 		
-		for (String city:cCodes) {
-			System.out.println(city);
-		}
+		//for (String city:cCodes) {
+//			System.out.println(city);
+//		}
 	
 	
 	}
