7fea86a92678acb65ebec1c5cac966b059e1d100
phobo3s <bberkkilinc@gmail.com>
2022-02-19 17:00:03 +0300

    springboot intro

---
+++ intro/src/main/java/com/berkcorp/intro/App.java
@@ -0,0 +1,13 @@
+package com.berkcorp.intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
